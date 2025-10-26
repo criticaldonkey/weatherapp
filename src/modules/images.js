38c90f0ac1fe4df0e60d7d3b80f959eb312f1524
criticaldonkey <shawn.petsch@gmail.com>
2025-10-26 17:55:31 -0600

    added base package

---
+++ src/modules/images.js
@@ -0,0 +1,8 @@
+// the general template for loading images below:
+
+// import imageFile from "./pathhere";
+
+// const image = document.createElement("img");
+// image.src = imageFile;
+
+// document.body.appendChild(image);
