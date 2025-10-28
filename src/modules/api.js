88c9893c42f3f823e0127fbab45ae5dead42ce2d
criticaldonkey <shawn.petsch@gmail.com>
2025-10-27 19:34:55 -0600

    commit base package

---
+++ src/modules/api.js
@@ -0,0 +1,19 @@
+const API_URL = "";
+const API_KEY = "";
+
+const weatherObj = {};
+
+async function getWeatherData(location) {
+    try {
+        // append location data from user or user's location to fetch string
+        // store response to weatherObj
+        
+    }
+    catch {
+        // handle errors
+    };
+        // validate object shape
+        // compartmentalize weatherObj to currentConditions, sunCycle, forecast, styleMgr objects and send to modules. 
+};
+
+export { getWeatherData }
